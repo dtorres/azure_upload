c9f388e1b298fe3a1d1a4862068411becf6d62f7
Diego Torres <warorface@gmail.com>
2017-08-19 22:24:30 +0200

    Added logger and checks for requirements

--- lib/azure_upload.rb
+++ lib/azure_upload.rb
@@ -7,11 +7,14 @@ require 'azure_mgmt_cdn'
 require 'pathname'
 require 'mime/types'
 require 'yaml'
+require 'logger'
 
 module AzureUpload
     
     MAX_PROCESS = 10
+    DEFAULT_CONFIG_FILE = ".azure_upload.yml"
     @config = {}
+    @logger = Logger.new(STDOUT)
     
     def self.cache_paths(uploader)
         container_dir = "/" + uploader.container_name + "/"
@@ -20,20 +23,53 @@ module AzureUpload
 
     def self.configure(config_path)
         file = File.open(config_path)
-        @config.merge!(file)
+        hash = YAML.load(file)
+        @config.merge!(hash) if hash.is_a? Hash
     end
     
+    def self.ensure_required_config(config)
+        failed = false
+        ["client_id", "subscription_id", "private_key", "tenant_id"].each do |key|
+          unless config[key].nil?
+            return
+          end
+          @logger.error "'#{key}' could not be found" 
+          failed = true
+        end
+        if failed
+          @logger.error "Provide the necessary keys by calling `configure`"
+          @logger.error "Alternatively they will be fetched from '#{DEFAULT_CONFIG_FILE}' in your home directory"
+        end
+        failed
+    end
+    
+    
+    
     def self.bust_cache(paths, res_group_name = nil, profile_name = nil, endpoint_name = nil)
-        configure("~/.azure_upload.yml") if @config.empty?
+        configure(File.expand_path("~/"+DEFAULT_CONFIG_FILE)) if @config.empty?
+        
         client_id = @config["client_id"]
         sub_id = @config["subscription_id"]
         cdn_key = @config["private_key"]
         tenant_id = @config["tenant_id"]
 
-        cdn_config = @config["CDN"]
-        res_group_name ||= cdn_config["resource_group"]
-        profile_name ||= cdn_config["profile"]
-        endpoint_name ||= cdn_config["endpoint"]
+        cdn_config = @config["CDN"] || {}
+        cdn_errors = []
+        res_group_name ||= cdn_config["resource_group"] || cdn_errors.push("Resource group not specified")
+        profile_name ||= cdn_config["profile"] || cdn_errors.push("Profile not specified")
+        endpoint_name ||= cdn_config["endpoint"] || cdn_errors.push("Endpoint not specified")
+        
+        return_early = false
+        unless cdn_errors.empty?
+          cdn_errors.each(&@logger.method(:error))
+          @logger.error "Provide the necessary arguments in `bust_cache`"
+          @logger.error "Alternatively they will be fetched from '#{DEFAULT_CONFIG_FILE}' in your home directory. Set a hash under the key 'CDN'"
+          return_early = true
+        end
+        return_early = ensure_required_config(@config) || return_early
+        if return_early
+          return
+        end
         
         provider = MsRestAzure::ApplicationTokenProvider.new(tenant_id, client_id, cdn_key)
     
@@ -48,10 +84,10 @@ module AzureUpload
             params = Azure::ARM::CDN::Models::PurgeParameters.new
             params.content_paths = sub_paths
             promise = cdn_client.endpoints.begin_purge_content_async(res_group_name, profile_name, endpoint_name, params)
-            puts promise.value!.response.status
+            @logger.info promise.value!.response.status
             start += max_paths
             if start < paths.count
-                puts "Sleeping for 3 mins before continuing u.u"
+                @logger.info "Sleeping for 3 mins before continuing"
                 sleep(3*60)
             end
         end
@@ -65,6 +101,8 @@ module AzureUpload
             @container_name = container_name
             @dir_to_upload = Pathname.new(dir_to_upload)
             @updated_paths = []
+            @logger = Logger.new(STDOUT)
+            @logger.level = Logger::INFO
             process_all = false
         end
         
@@ -96,7 +134,7 @@ module AzureUpload
                         next
                     end
                     if !process_all && (!latest_existing_date.nil? && maybe[:mtime] < latest_existing_date)
-                        puts "Breaking because date!"
+                        @logger.debug "Breaking because date!"
                         process_more = false
                         break
                     end
@@ -122,7 +160,7 @@ module AzureUpload
                     f = h[:path]
                     if File.directory? f
                         upload_dir(f, latest_existing_date)
-                        else
+                    else
                         mtime = h[:mtime]
                         upload_file(f, mtime, dir)
                     end
@@ -138,7 +176,7 @@ module AzureUpload
             relative = Pathname.new(path).relative_path_from(@dir_to_upload)
             mime = MIME::Types.type_for(File.basename(path))
             mime = mime[0] if mime.is_a? Array
-            puts path if mime.nil?
+
             local_hash = Digest::MD5.file(path).base64digest
             upload = false
             existed = false
@@ -146,11 +184,11 @@ module AzureUpload
                 remote_hash = @blobs.get_blob_properties(@container_name, relative).properties[:content_md5]
                 upload = local_hash != remote_hash
                 existed = true
-                rescue Azure::Core::Http::HTTPError => error
+            rescue Azure::Core::Http::HTTPError => error
                 upload = error.status_code == 404
             end
             if !upload
-                puts "Have date"
+                @logger.debug "Have date"
                 return {:date => mdate, :uploaded_path => nil }
             end
             puts "Processing file: #{path}"
